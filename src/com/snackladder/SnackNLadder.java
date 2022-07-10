784968ebe68beca6f1a9faed2f83b81837b062ae
HP <sachdevamohita5@gmail.com>
2022-07-10 12:32:36 +0530

    initial position of the player

---
+++ src/com/snackladder/SnackNLadder.java
@@ -0,0 +1,8 @@
+package com.snackladder;
+
+public class SnackNLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snack and Ladder game");
+        int position=0;
+    }
+}
